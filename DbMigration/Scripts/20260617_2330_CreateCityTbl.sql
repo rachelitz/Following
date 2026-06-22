13003c49304fb9edf12af553fccd1fe241dfabab
Chani <chanic668@gmail.com>
2026-06-23 00:47:31 +0300

    tables

---
+++ DbMigration/Scripts/20260617_2330_CreateCityTbl.sql
@@ -0,0 +1,18 @@
+USE [Following]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [City](
+	[CityCode] [int] IDENTITY(1,1) NOT NULL,
+	[CountryCode] [int] NULL,
+	[CityNameHebrew] [nvarchar](50) NULL,
+	[CityNameEnglish] [nvarchar](50) NULL,
+ CONSTRAINT [PK_tbl_city] PRIMARY KEY CLUSTERED 
+(
+	[CityCode] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
---
+++ FollowingDal/Model/City.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace FollowingDal.Model;
+
+[Table("City")]
+public partial class City
+{
+    [Key]
+    public int CityCode { get; set; }
+
+    public int? CountryCode { get; set; }
+
+    [StringLength(50)]
+    public string? CityNameHebrew { get; set; }
+
+    [StringLength(50)]
+    public string? CityNameEnglish { get; set; }
+}
